41e84ab888a352b2b6ffad348c8f36de85a7b7b1
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2024-12-03 16:51:00 -0400

    commit com background e testes

---
+++ scripts/camera.js
@@ -0,0 +1,6 @@
+const camera = document.getElementById("camera");
+const botao = document.getElementById("botaoCamera");
+
+botao.addEventListener("click",function(){
+    camera.style.visibility = "visible"
+});
