2e3f03500bdfb9e7085d333d2cb79dd7eb16df0b
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2024-12-04 17:13:33 -0400

    commit de hoje

--- scripts/camera.js
+++ scripts/camera.js
@@ -2,5 +2,11 @@ const camera = document.getElementById("camera");
 const botao = document.getElementById("botaoCamera");
 
 botao.addEventListener("click",function(){
-    camera.style.visibility = "visible"
+    if (camera.style.display == "none"){
+        camera.style.display = "flex"
+        camera.style.animation = "slide-rotate-hor-bottom 0.18s cubic-bezier(0.250, 0.460, 0.450, 0.940) reverse both"
+    }else{
+        camera.style.animation = "slide-rotate-hor-bottom 0.18s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
+        camera.style.display = "none"
+    }
 });
