d0fad501dbcf275d15bbf9d0158d75af1b2816e6
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2024-12-09 17:13:18 -0400

    relogio funcional

---
+++ scripts/tempo.js
@@ -0,0 +1,28 @@
+const relogio = document.getElementById("hour");
+const noite = document.getElementById("days");
+noiteNum = 2;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function nextNight(){
+    if (noiteNum==8){
+        return "Venceu!"
+    }
+    relogio.innerHTML = "12 AM";
+    noite.innerHTML = `Night ${noiteNum}`;
+    noiteNum++
+    timeFlow();
+}
+
+async function timeFlow(){
+    for (let hora = 1; hora != 7; hora++) {
+        await sleep(90000);
+        relogio.innerHTML = `${hora} AM`;
+    }
+    await sleep(5000)
+    nextNight();
+}
+
+timeFlow();
