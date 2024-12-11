2c79a324551b4d8dad05548af606e1356696e48c
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2024-12-11 15:58:55 -0400

    jumpscares feitos

--- scripts/jumpscare.js
+++ scripts/jumpscare.js
@@ -1,4 +1,8 @@
 let jumpChance = 0;
+const freddyJump = document.getElementById("freddyJump");
+const bonnieJump = document.getElementById("bonnieJump");
+const chicaJump = document.getElementById("chicaJump");
+const foxyJump = document.getElementById("foxyJump");
 
 function getChance(){
     jumpChance = Math.floor(Math.random() * 200);
